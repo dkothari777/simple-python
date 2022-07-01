b098c48cd1bda2919ab8a28f6728f68befb0fd0c
Darshan Kothari <dkothari777@gmail.com>
2022-07-01 14:32:32 -0400

    Add more cleaner script testing reading all the stdouts and stderrs

--- test/test_main.py
+++ test/test_main.py
@@ -1,4 +1,15 @@
 import pytest
+import runpy
+import pathlib
 from simplepython import main
-def test_init():
-    assert main.main() =="Hello World!"
+
+
+
+def test_init(capsys):
+    scripts = pathlib.Path('../simplepython/main.py').resolve()
+    runpy.run_path(scripts, run_name='__main__')
+    captured = capsys.readouterr()
+    assert captured.out == "Hello World!\n"
+
+def test_main():
+    assert main.main() == "Hello World!"
