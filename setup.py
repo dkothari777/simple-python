5c5349d9ca0228e916adf9064b371565563b7a5c
Darshan Kothari <dkothari777@gmail.com>
2022-07-01 10:03:11 -0400

    Add setup to simple-python

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="simple-python",
+    version="0.0.1",
+    author="Darshan Kothari",
+    author_email="dkothari777@gmail.com",
+    description="Simple Package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
